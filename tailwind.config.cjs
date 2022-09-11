1280674641922fea0747169a017d8df346a7d540
Ash <maybork@pm.me>
2022-09-11 20:10:42 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'gothic': ["'DwarvenAxe'"],
+			},
+			colors: {
+				'radiant': '#f5ebc6',
+				'blight': '#bdc241',
+				'bleed': '#c41c1c',
+				'stun': '#c99c45',
+				'debuff': '#c3630f',
+				'move': '#4c8ba2'
+			},
+			backgroundImage: {
+				'index': "url('/index.webp')",
+				'backdrop': "url('/background.webp')",
+			}
+		},
+	},
+	plugins: [],
+}
