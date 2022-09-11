94a372109589c8ee2194be10d483d98d9665cc6c
Ash <maybork@pm.me>
2022-09-11 20:22:55 +0200

    add Lato

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Lato', 'sans-serif'],
+		},
 		extend: {
 			fontFamily: {
 				'gothic': ["'DwarvenAxe'"],
